14cbc2f4eb41d113d687a882c8606f952ff50989
inb <inorton@gmail.com>
2019-03-01 12:18:57 +0000

    fix #23 enable ctest and fix errors finding windows test results
    bump to 3.0.8

--- VS2015/build-x64.bat
+++ VS2015/build-x64.bat
@@ -2,7 +2,5 @@ mkdir windows-build
 cd windows-build
 cmake -G "Visual Studio 14 2015 Win64" .. || exit \b
 cmake --build . --config Release || exit \b
+ctest -C Release  --output-on-failure || exit \b
 
-cd CUnit
-
-Release\cunit_test.exe || exit \b
--- VS2015/test-x64.bat
+++ VS2015/test-x64.bat
@@ -1,19 +1,8 @@
 @echo on
 cd windows-build
-cd Examples\CI\Release
 
-cicd-pass-plain || exit /b
-cicd-pass-setupfuncs || exit /b
+cd Examples\CI
+dir
 
-echo cicd-pass-plain-Results.xml
-type cicd-pass-plain-Results.xml
-
-echo ----
-
-echo cicd-pass-setupfuncs-Results.xml
-type cicd-pass-setupfuncs-Results.xml
-
-echo ----
-
-python -m junit2htmlreport --summary-matrix cicd-pass-plain-Results.xml cicd-pass-setupfuncs-Results.xml
+python -m junit2htmlreport --summary-matrix cicd-pass-plain.exe-Results.xml cicd-pass-setupfuncs.exe-Results.xml cicd-multi-suite.exe-Results.xml || exit /b
 
